03b88316ea3b59569187e0ce2f15ec49f842663d
Stephanie <55038687+stephypy@users.noreply.github.com>
2019-09-08 09:46:24 -0600

    completion

--- Main.py
+++ Main.py
@@ -4,12 +4,13 @@
 # Instructor: Diego Aguirre
 # TA: Gerardo
 # Date of last modification: September 8, 2019
-# Purpose:
+# Purpose: Given a file containing users, salt values, and hashed passwords, create a recursive method 
+#          that uses brute force to generate all users passwords. 
 
 import hashlib
 import itertools
 
-
+# Provided function for hashing
 def hash_with_sha256(str):
     hash_object = hashlib.sha256(str.encode('utf-8'))
     hex_dig = hash_object.hexdigest()
@@ -17,8 +18,6 @@ def hash_with_sha256(str):
 
 
 def hack_password(pass_length):
-    # Create all the possible combinations of the following digits of n length
-    num_combination = list(itertools.product(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], repeat=pass_length))
     # List that will contain corresponding passwords for users
     passwords = []
     # Check parameter's data type
@@ -27,35 +26,52 @@ def hack_password(pass_length):
       return
     # Password will be at least 3 digits long and at most 7 digits long
     if pass_length < 3 or pass_length > 7:
-        return passwords.sort
+        passwords.sort()
+        return passwords
+    # Create all the possible combinations of the following digits of n length
+    num_combination = list(itertools.product(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], repeat=pass_length))
+    count = 0
+    # It will iterate each num_combination of length pass_length through each line in the file
     for i in num_combination:
         with open("password_file.txt") as f:
             for curr_line in f:
+                count += 1
                 elements = curr_line.split(",")
+                # This is the corresponding hashed line of the current line
                 hash_element = elements[2].replace('\n', '')
+                # This is a possible password for the user at current line
                 curr_num = ''.join(i)
+                # Call method to hash the possible password plus salt value
                 hashline = hash_with_sha256(curr_num + str(elements[1]))
+                # When a password is found, add it to the list of passwords
                 if hashline == hash_element:
                     passwords.append(elements[0] + ': ' + curr_num)
-    passwords += hack_password(pass_length + 1)
+                # At the end of the file, make a recursive call by incrementing the length of pass_length
+                if count ==  100:
+                    passwords += hack_password(pass_length + 1
+    passwords.sort()
+    return passwords
 
 
 def test_cases():
     # Pass a higher parameter than expected:
-    hack_password(8)
-    
-    #Pass a lower parameter than expected:
-    hack_password(0)
-    
-    #Pass an invalid parameter i.e.: a string, boolean, etc
-    hack_password('hello')
+    higher = hack_password(8)
+    print(higher)
+    print('')
+    # Pass a lower parameter than expected:
+    lower = hack_password(0)
+    print(lower)
+    print('')
+    # Pass an invalid parameter i.e.: a string, float, etc
+    print(hack_password(4.5))
+    print('')
   
   
 def main():
     # Test cases:
     test_cases()
     # Normal run of the program:
-    hack_password(3)
+    print(hack_password(3))
 
 
 main()
