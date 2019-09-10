c844e2729bc9e9d74c4f979394ca9d1cf04012f0
Stephanie <55038687+stephypy@users.noreply.github.com>
2019-09-09 20:25:00 -0600

    Completion

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@
 #          that uses brute force to generate all users passwords. 
 
 import hashlib
-
+import time
 
 def hash_with_sha256(str):
     hash_object = hashlib.sha256(str.encode('utf-8'))
@@ -42,15 +42,43 @@ def get_combination(size, comb):
             get_combination(size - 1, new_comb)
 
 
-def hack_file():
-    for size in range(3, 8):
+def hack_file(start, end):
+    # Check parameter's data type
+    if not isinstance(start, int) or not isinstance(end, int):
+        print('Invalid input. Please try again')
+        return
+    if start < 3 or end > 7:
+        print('Invalid input. Please try again')
+        return
+    for size in range(start, end + 1):
         get_combination(size, '')
 
 
+def test_cases():
+    # Invalid data types
+    start = time.time()
+    hack_file(3.0, 7.0)
+    end = time.time()
+    print('Running time for invalid data type:', end - start, 'seconds\n')
+    # Small starting size
+    start = time.time()
+    hack_file(2, 7)
+    end = time.time()
+    print('Running time for invalid data type:', end - start, 'seconds\n')
+    # Large end size
+    start = time.time()
+    hack_file(3, 8)
+    end = time.time()
+    print('Running time for invalid data type:', end - start, 'seconds\n')
+
+
 def main():
-    # Function that finds the passwords of the given file
-    hack_file()
+    # Normal run of the program
+    start = time.time()
+    hack_file(3, 7)
+    end = time.time()
+    print('Running time for normal run: ', end - start, 'seconds\n')
+    test_cases()
 
 
 main()
-
